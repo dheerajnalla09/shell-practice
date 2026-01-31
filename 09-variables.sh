6a2947192ce63664f7fefa947103c7218f1bed42
Dheeraj <dheeraj.azure@aol.com>
2026-01-31 23:54:54 +0530

    actions

--- 09-variables.sh
+++ 09-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NUMBER=25
+NUMBER=$1
 
-if [$NUMBER -gt 20]; then
-echo "given number:$NUMBER is greater than 20"
+if [ $NUMBER -gt 20]; then
+    echo "given number: $NUMBER is greater than 20"
 fi
