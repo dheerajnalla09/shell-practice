6d04bb866b851faaf22d4bebce0f218f173b0922
Dheeraj <dheeraj.azure@aol.com>
2026-01-31 20:01:07 +0530

    actions

---
+++ 07-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##### special ####
+
+
+echo "all args paased to script: $@"..
+echo "Number of vars paased to script: $#"
+echo "script name: $0"
+echo "present directory: $PWD"
+echo "PID of this script: $$"
+echo "PID of recently exected background process: $!"
