8a3e281c0fea226729862155f4ee1a811377af01
Dheeraj <dheeraj.azure@aol.com>
2026-02-08 12:52:30 +0530

    actions

--- mail.sh
+++ mail.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-
 TO_ADDRESS=$1
-Subject=$2
+SUBJECT=$2
 MESSAGE_BODY=$3
 ALERT_TYPE=$4
 SERVER_IP=$5
 TO_TEAM=$6
+# FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
 
 {
-echo "To: dheerajnalla92@gmail.com"
-echo "Subject: hello world"
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT"
 echo "Content-Type: text/html"
 echo ""
-echo "Hi"
-} | msmtp "dheerajnalla92@gmail.com"
+echo "$FINAL_MESSAGE"
+} | msmtp "$TO_ADDRESS"
